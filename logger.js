c1db12e3d22ca7cb58a7486e0e83d0f2b6759355
Rajesh Patel <RajeshMPatel@gmail.com>
2022-01-23 10:57:46 -0800

    Added driver, logger and order class

---
+++ logger.js
@@ -0,0 +1,17 @@
+
+// Add abstraction layer for logging so we can have flexibility on
+// where to log. Here we are logging to console. We could
+// choose to log to a file instead.
+
+// NOTE: Ideally you want to implement log levels. I will if I have 
+// time. The important thing here is the abstraction.
+
+// We can also use bunyan and other sophisticated loggers.
+
+function log(message) {
+    //const now = Date.now();
+    //console.log(`${now}: ${message}`);
+    console.log(message);
+}
+
+module.exports.log = log;
